8f73c25fa83e7d33f6b337ea2f81f7f2d530a766
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2023-03-13 22:02:19 +0500

    add SECURE_CROSS_ORIGIN_OPENER_POLICY

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = BASE_DIR.joinpath("media")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
